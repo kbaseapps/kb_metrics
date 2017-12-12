821991ce9bedda11dfa3dca302c79f05561ce66b
qzzhang <qz_zhang@yahoo.com>
2017-12-12 13:46:07 -0800

    rmed comments

--- lib/kb_Metrics/metricsdb_controller.py
+++ lib/kb_Metrics/metricsdb_controller.py
@@ -40,7 +40,6 @@ class MetricsMongoDBController:
 
     def __init__(self, config):
         #pprint("initializing mdb......")
-        pprint(config)
         # first grab the admin list
         self.adminList = []
         if 'admin-users' in config:
