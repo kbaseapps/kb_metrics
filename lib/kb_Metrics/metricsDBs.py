f1be22ef90425ae508df80bca21470b9d8441af0
Qizhi Zhang <qzzhang@users.noreply.github.com>
2018-03-02 19:07:00 -0600

    shortened lines pointed out by pycodestyle (pep8)

--- lib/kb_Metrics/metricsDBs.py
+++ lib/kb_Metrics/metricsDBs.py
@@ -16,7 +16,7 @@ class MongoMetricsDBI:
 
     _AUTH2_USERS = 'users'  # auth2.users
     _MT_USERS = 'users'  # 'test_users'#metrics.users
-    _MT_DAILY_ACTIVITIES = 'daily_activities'  # 'test_activities'#metrics.daily_activities
+    _MT_DAILY_ACTIVITIES = 'daily_activities'
     _MT_NARRATIVES = 'narratives'  # metrics.narratives
 
     _USERPROFILES = 'profiles'  # user_profile_db.profiles
@@ -34,8 +34,9 @@ class MongoMetricsDBI:
         self.metricsDBs = dict()
         for m_db in mongo_dbs:
             # create the client and authenticate
-            self.mongo_clients[m_db] = MongoClient("mongodb://" + mongo_user + ":" + mongo_psswd
-                                                   + "@" + mongo_host + "/" + m_db)
+            self.mongo_clients[m_db] = MongoClient(
+                "mongodb://" + mongo_user + ":" + mongo_psswd +
+                "@" + mongo_host + "/" + m_db)
             # grab a handle to the database
             self.metricsDBs[m_db] = self.mongo_clients[m_db][m_db]
 
@@ -54,11 +55,13 @@ class MongoMetricsDBI:
         update_ret = None
         try:
             # return an instance of UpdateResult(raw_result, acknowledged)
-            update_ret = self.mt_users.update_one(upd_filter, upd_op, upsert=True)
+            update_ret = self.mt_users.update_one(upd_filter,
+                                                  upd_op, upsert=True)
         except BulkWriteError as bwe:
-            #print("mt_users.update errored\n:")
+            # print("mt_users.update errored\n:")
             # pprint(bwe.details['writeErrors'])
-            panic = filter(lambda x: x['code'] != 11000, bwe.details['writeErrors'])
+            panic = filter(lambda x: x['code'] != 11000,
+                           bwe.details['writeErrors'])
             if len(panic) > 0:
                 print "really panic"
                 raise
@@ -77,14 +80,17 @@ class MongoMetricsDBI:
                   "$set": upd_data}
 
         # grab handle(s) to the database collection(s) targeted
-        self.mt_coll = self.metricsDBs['metrics'][MongoMetricsDBI._MT_DAILY_ACTIVITIES]
+        self.mt_coll = self.metricsDBs['metrics'][
+            MongoMetricsDBI._MT_DAILY_ACTIVITIES]
         update_ret = None
         try:
             # return an instance of UpdateResult(raw_result, acknowledged)
-            update_ret = self.mt_coll.update_one(upd_filter, upd_op, upsert=True)
+            update_ret = self.mt_coll.update_one(upd_filter,
+                                                 upd_op, upsert=True)
         except BulkWriteError as bwe:
             # pprint(bwe.details['writeErrors'])
-            panic = filter(lambda x: x['code'] != 11000, bwe.details['writeErrors'])
+            panic = filter(lambda x: x['code'] != 11000,
+                           bwe.details['writeErrors'])
             if len(panic) > 0:
                 print "really panic"
                 raise
@@ -192,15 +198,17 @@ class MongoMetricsDBI:
             {"$match": match_filter},
             {"$project": {"year_mod": "$_id.year_mod", "month_mod": "$_id.month_mod",
                           "day_mod": "$_id.day_mod", "username": "$_id.username", "_id": 0}},
-            {"$group": {"_id": {"year_mod": "$year_mod", "month_mod": "$month_mod", "day_mod": "$day_mod",
-                                "username": "$username"}}},
+            {"$group": {"_id": {"year_mod": "$year_mod", "month_mod": "$month_mod",
+                                "day_mod": "$day_mod", "username": "$username"}}},
             {"$group": {"_id": {"year_mod": "$_id.year_mod", "month_mod": "$_id.month_mod",
                                 "day_mod": "$_id.day_mod"}, "numOfUsers": {"$sum": 1}}},
-            {"$sort": {"_id.year_mod": ASCENDING, "_id.month_mod": ASCENDING, "_id.day_mod": ASCENDING}},
-            {"$project": {"yyyy-mm-dd": {"$concat": [{"$substr": ["$_id.year_mod", 0, -1]}, '-',
-                                                     {"$substr": [
-                                                         "$_id.month_mod", 0, -1]}, '-',
-                                                     {"$substr": ["$_id.day_mod", 0, -1]}]},
+            {"$sort": {"_id.year_mod": ASCENDING,
+                       "_id.month_mod": ASCENDING,
+                       "_id.day_mod": ASCENDING}},
+            {"$project": {"yyyy-mm-dd": {"$concat":
+                                         [{"$substr": ["$_id.year_mod", 0, -1]}, '-',
+                                          {"$substr": ["$_id.month_mod", 0, -1]}, '-',
+                                          {"$substr": ["$_id.day_mod", 0, -1]}]},
                           "numOfUsers":1, "_id":0}}
         ]
 
@@ -261,11 +269,17 @@ class MongoMetricsDBI:
         # Define the pipeline operations
         pipeline = [
             {"$match": {"moddate": {"$gte": minTime, "$lte": maxTime}}},
-            {"$project": {"year_mod": {"$year": "$moddate"}, "month_mod": {"$month": "$moddate"},
-                          "date_mod": {"$dayOfMonth": "$moddate"}, "obj_name": "$name",
-                          "obj_id": "$id", "obj_version": "$numver", "ws_id": "$ws", "_id": 0}},
-            {"$group": {"_id": {"ws_id": "$ws_id", "year_mod": "$year_mod",
-                                "month_mod": "$month_mod", "day_mod": "$date_mod"},
+            {"$project": {"year_mod": {"$year": "$moddate"},
+                          "month_mod": {"$month": "$moddate"},
+                          "date_mod": {"$dayOfMonth": "$moddate"},
+                          "obj_name": "$name",
+                          "obj_id": "$id",
+                          "obj_version": "$numver",
+                          "ws_id": "$ws", "_id": 0}},
+            {"$group": {"_id": {"ws_id": "$ws_id",
+                                "year_mod": "$year_mod",
+                                "month_mod": "$month_mod",
+                                "day_mod": "$date_mod"},
                         "obj_numModified": {"$sum": 1}}},
             {"$sort": {"_id": ASCENDING}}
         ]
@@ -278,12 +292,17 @@ class MongoMetricsDBI:
         # Define the pipeline operations
         pipeline = [
             {"$match": {"moddate": {"$gte": minTime, "$lte": maxTime}}},
-            {"$project": {"year_mod": {"$year": "$moddate"}, "month_mod": {"$month": "$moddate"},
-                          "date_mod": {"$dayOfMonth": "$moddate"}, "owner": 1, "ws_id": "$ws",
+            {"$project": {"year_mod": {"$year": "$moddate"},
+                          "month_mod": {"$month": "$moddate"},
+                          "date_mod": {"$dayOfMonth": "$moddate"},
+                          "owner": 1, "ws_id": "$ws",
                           "numObj": 1, "_id": 0}},
-            {"$group": {"_id": {"username": "$owner", "year_mod": "$year_mod",
-                                "month_mod": "$month_mod", "day_mod": "$date_mod"},
-                        "ws_numModified": {"$sum": 1}, "ws_numObjs": {"$sum": "$numObj"}}},
+            {"$group": {"_id": {"username": "$owner",
+                                "year_mod": "$year_mod",
+                                "month_mod": "$month_mod",
+                                "day_mod": "$date_mod"},
+                        "ws_numModified": {"$sum": 1},
+                        "ws_numObjs": {"$sum": "$numObj"}}},
             {"$sort": {"_id": ASCENDING}}
         ]
         # grab handle(s) to the database collections needed and retrieve a MongoDB cursor
@@ -295,11 +314,15 @@ class MongoMetricsDBI:
         # Define the pipeline operations
         pipeline = [
             {"$match": {"moddate": {"$gte": minTime, "$lte": maxTime}}},
-            {"$project": {"year": {"$year": "$moddate"}, "month": {"$month": "$moddate"},
-                          "date": {"$dayOfMonth": "$moddate"}, "owner": 1, "ws_id": "$ws",
+            {"$project": {"year": {"$year": "$moddate"},
+                          "month": {"$month": "$moddate"},
+                          "date": {"$dayOfMonth": "$moddate"},
+                          "owner": 1, "ws_id": "$ws",
                           "numObj": 1, "meta": 1, "_id": 0}},
-            {"$group": {"_id": {"username": "$owner", "year": "$year",
-                                "month": "$month"}, "year_mon_user_logins": {"$sum": 1}}},
+            {"$group": {"_id": {"username": "$owner",
+                                "year": "$year",
+                                "month": "$month"},
+                        "year_mon_user_logins": {"$sum": 1}}},
             {"$sort": {"_id": ASCENDING}}
         ]
         # grab handle(s) to the database collections needed and retrieve a MongoDB cursor
@@ -311,13 +334,19 @@ class MongoMetricsDBI:
         # Define the pipeline operations
         pipeline = [
             {"$match": {"moddate": {"$gte": minTime, "$lte": maxTime}}},
-            {"$project": {"year": {"$year": "$moddate"}, "month": {"$month": "$moddate"},
-                          "date": {"$dayOfMonth": "$moddate"}, "owner": 1, "ws_id": "$ws",
+            {"$project": {"year": {"$year": "$moddate"},
+                          "month": {"$month": "$moddate"},
+                          "date": {"$dayOfMonth": "$moddate"},
+                          "owner": 1, "ws_id": "$ws",
                           "numObj": 1, "meta": 1, "_id": 0}},
-            {"$group": {"_id": {"username": "$owner", "year": "$year",
-                                "month": "$month"}, "count_user_ws_logins": {"$sum": 1}}},
-            {"$group": {"_id": {"year": "$_id.year", "month": "$_id.month"},
-                        "year_mon_total_logins": {"$sum": "$count_user_ws_logins"}}},
+            {"$group": {"_id": {"username": "$owner",
+                                "year": "$year",
+                                "month": "$month"},
+                        "count_user_ws_logins": {"$sum": 1}}},
+            {"$group": {"_id": {"year": "$_id.year",
+                                "month": "$_id.month"},
+                        "year_mon_total_logins": {
+                            "$sum": "$count_user_ws_logins"}}},
             {"$sort": {"_id": ASCENDING}}
         ]
         # grab handle(s) to the database collections needed and retrieve a MongoDB cursor
@@ -329,11 +358,15 @@ class MongoMetricsDBI:
         # Define the pipeline operations
         pipeline = [
             {"$match": {"moddate": {"$gte": minTime, "$lte": maxTime}}},
-            {"$project": {"year": {"$year": "$moddate"}, "month": {"$month": "$moddate"},
-                          "date": {"$dayOfMonth": "$moddate"}, "owner": 1, "ws_id": "$ws",
+            {"$project": {"year": {"$year": "$moddate"},
+                          "month": {"$month": "$moddate"},
+                          "date": {"$dayOfMonth": "$moddate"},
+                          "owner": 1, "ws_id": "$ws",
                           "numObj": 1, "meta": 1, "_id": 0}},
-            {"$group": {"_id": {"username": "$owner", "year": "$year",
-                                "month": "$month"}, "count_user_ws": {"$sum": 1}}},
+            {"$group": {"_id": {"username": "$owner",
+                                "year": "$year",
+                                "month": "$month"},
+                        "count_user_ws": {"$sum": 1}}},
             {"$sort": {"_id": ASCENDING}}
         ]
         # grab handle(s) to the database collections needed and retrieve a MongoDB cursor
@@ -346,11 +379,15 @@ class MongoMetricsDBI:
         pipeline = [
             {"$match": {"moddate": {"$gte": minTime, "$lte": maxTime},
                         "meta": {"$exists": True, "$not": {"$size": 0}}}},
-            {"$project": {"year": {"$year": "$moddate"}, "month": {"$month": "$moddate"},
-                          "date": {"$dayOfMonth": "$moddate"}, "owner": 1, "ws_id": "$ws",
+            {"$project": {"year": {"$year": "$moddate"},
+                          "month": {"$month": "$moddate"},
+                          "date": {"$dayOfMonth": "$moddate"},
+                          "owner": 1, "ws_id": "$ws",
                           "numObj": 1, "meta": 1, "_id": 0}},
-            {"$group": {"_id": {"username": "$owner", "year": "$year",
-                                "month": "$month"}, "count_user_narratives": {"$sum": 1}}},
+            {"$group": {"_id": {"username": "$owner",
+                                "year": "$year",
+                                "month": "$month"},
+                        "count_user_narratives": {"$sum": 1}}},
             {"$sort": {"_id": ASCENDING}}
         ]
         # grab handle(s) to the database collections needed and retrieve a MongoDB cursor
@@ -362,11 +399,16 @@ class MongoMetricsDBI:
         # Define the pipeline operations
         pipeline = [
             {"$match": {"moddate": {"$gte": minTime, "$lte": maxTime}}},
-            {"$project": {"year": {"$year": "$moddate"}, "month": {"$month": "$moddate"},
-                          "date": {"$dayOfMonth": "$moddate"}, "owner": 1, "ws_id": "$ws",
+            {"$project": {"year": {"$year": "$moddate"},
+                          "month": {"$month": "$moddate"},
+                          "date": {"$dayOfMonth": "$moddate"},
+                          "owner": 1, "ws_id": "$ws",
                           "numObj": 1, "meta": 1, "_id": 0}},
-            {"$group": {"_id": {"username": "$owner", "year": "$year",
-                                "month": "$month"}, "count_user_numObjs": {"$sum": "$numObj"}}},
+            {"$group": {"_id": {"username": "$owner",
+                                "year": "$year",
+                                "month": "$month"},
+                        "count_user_numObjs": {
+                            "$sum": "$numObj"}}},
             {"$sort": {"_id": ASCENDING}}
         ]
 
@@ -390,9 +432,10 @@ class MongoMetricsDBI:
         # Define the pipeline operations
         pipeline = [
             {"$match": {"del": False, "meta": {"$exists": True, "$not": {"$size": 0}}}},
-            {"$project": {"username": "$owner", "workspace_id": "$ws", "name": 1, "meta": 1,
-                          "deleted": "$del", "desc": 1, "numObj": 1, "last_saved_at": "$moddate",
-                          "_id": 0}}
+            {"$project": {"username": "$owner", "workspace_id": "$ws",
+                          "name": 1, "meta": 1,
+                          "deleted": "$del", "desc": 1, "numObj": 1,
+                          "last_saved_at": "$moddate", "_id": 0}}
         ]
         # grab handle(s) to the database collections needed and retrieve a MongoDB cursor
         self.kbworkspaces = self.metricsDBs['workspace'][MongoMetricsDBI._WS_WORKSPACES]
@@ -403,8 +446,9 @@ class MongoMetricsDBI:
         # Define the pipeline operations
         pipeline = [
             {"$match": {"del": False, "moddate": {"$gte": minTime, "$lte": maxTime}}},
-            {"$project": {"moddate": 1, "workspace_id": "$ws", "object_id": "$id",
-                          "object_name": "$name", "object_version": "$numver", "deleted": "$del",
+            {"$project": {"moddate": 1, "workspace_id": "$ws",
+                          "object_id": "$id", "object_name": "$name",
+                          "object_version": "$numver", "deleted": "$del",
                           "latest": 1, "_id": 0}}
         ]
         # grab handle(s) to the database collections needed and retrieve a MongoDB cursor
@@ -503,9 +547,11 @@ class MongoMetricsDBI:
 
         pipeline = [
             match_cond,
-            {"$project": {"username": "$user", "email": "$email", "full_name": "$display",
-                          "signup_at": "$create", "last_signin_at": "$login", "roles": 1,
-                          "_id": 0}},
+            {"$project": {"username": "$user", "email": "$email",
+                          "full_name": "$display",
+                          "signup_at": "$create",
+                          "last_signin_at": "$login", 
+                          "roles": 1, "_id": 0}},
             {"$sort": {"signup_at": 1}}
         ]
 
@@ -586,9 +632,8 @@ class MongoMetricsDBI:
             'created': 1,  # datetime.datetime(2015, 1, 9, 19, 36, 8, 561000)
             'started': 1,
             'updated': 1,
-            # e.g., "queued", "ws.18657.obj.1", "Initializing", "canceled by user", etc.
             'status': 1,
-            'progtype': 1,  # e.g., "percent", "task"
+            'progtype': 1,
             'authparam': 1,  # "DEFAULT" or workspace_id
             'authstrat': 1,  # "DEFAULT" or "kbaseworkspace"
             'complete': 1,
